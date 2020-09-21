85184b5a5ff411c7daa8e80dc5f71f055f909b07
Manish Patel <mpatel27@bu.edu>
2020-09-21 16:09:17 -0400

    added support to handle floats too

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,22 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    matrix = []
+    with open(csv_file_path, "r+") as f:
+        lines = f.readlines()
+        for row in lines:
+            entries = row.split(",")
+            toAdd = []
+            for i in range(len(entries)):
+                entries[i] = entries[i].rstrip('\n')
+                if entries[i].isnumeric():
+                    toAdd.append(int(entries[i]))
+                else:
+                    try:
+                        toAdd.append(float(entries[i]))
+                    except ValueError:
+                        toAdd.append(entries[i])
+            matrix.append(toAdd)
+        f.close()
+    return matrix
+
