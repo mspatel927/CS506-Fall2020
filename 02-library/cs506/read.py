21c12d9de40d76d8a7a7aa7ab19470741959dac2
Manish Patel <mpatel27@bu.edu>
2020-09-21 15:46:26 -0400

    initial commit

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,17 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    matrix = []
+    with open(csv_file_path, "r+") as f:
+        lines = f.readlines()
+        for row in lines:
+            entries = row.split(",")
+            toAdd = []
+            for i in range(len(entries)):
+                if entries[i].isnumeric():
+                    toAdd.append(int(entries[i]))
+                else:
+                    toAdd.append(entries[i])
+            matrix.append(toAdd)
+        f.close()
+    return matrix
